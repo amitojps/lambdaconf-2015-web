2a395f92e4e29be4bf76642e977a53a79332dc72
amitojps <amitojps@gmail.com>
2017-01-12 16:33:55 -0800

    Exercise 1

--- ex1-json/src/Main.hs
+++ ex1-json/src/Main.hs
@@ -1,20 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Data.Aeson hiding (json)
 import Web.Spock.Safe
+import GHC.Generics
 
 data Person = Person { name    :: String
                      , age     :: Integer
                      , address :: String
-                     }
+                     } deriving Generic
+
+instance ToJSON Person
 
 listPeople :: [Person]
 listPeople = [ Person "Fulanito"  30 "Boulder, CO"
              , Person "Menganito" 25 "Madrid, ES"
              , Person "Zutano"    35 "Utrecht, NL"
              ]
+
+data Task = Task { title :: String
+                 , description :: String
+                 , person :: Person}
 
 main :: IO ()
 main = runSpock 8080 $ spockT id $ do
@@ -23,4 +31,6 @@ main = runSpock 8080 $ spockT id $ do
     json $ object [ "hello" .= String name ]
   -- /allow/:age
   get ("allow" <//> var) $ \(age :: Integer) ->
-    error "Implement me!"
+    json $ object [ "allowed" .= Bool True]
+  get ("person" <//> var) $
+    json . (!!) listPeople
